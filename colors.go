8cd187f89bc2716b294eabe145a2d0625959536e
Wellington Gadelha <contato.informeai@gmail.com>
2021-08-11 08:23:35 -0300

    create first usable code.

--- colors.go
+++ colors.go
@@ -2,12 +2,32 @@ package colors
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 //consts for implementations.
+const prefix = "\x1b"
+
+//output default os.Stdout
+var output = os.Stdout
+
+//Attribute is base sgr code for implementations ansi colors.
+type Attribute int
+
+// base attributes
 const (
-	prefix = "\x1b"
-	bright = 60
+	Reset Attribute = iota
+	Bold
+	Faint
+	Italic
+	Underline
+	BlinkSlow
+	BlinkRapid
+	ReverseVideo
+	Concealed
+	CrossedOut
 )
 
 //colors foreground.
@@ -22,6 +42,18 @@ const (
 	FgWhite
 )
 
+// foreground hi-intensity text colors
+const (
+	FgHiBlack Attribute = iota + 90
+	FgHiRed
+	FgHiGreen
+	FgHiYellow
+	FgHiBlue
+	FgHiMagenta
+	FgHiCyan
+	FgHiWhite
+)
+
 //colors background
 const (
 	BgBlack = iota + 40
@@ -34,24 +66,85 @@ const (
 	BgWhite
 )
 
+// background hi-intensity text colors
+const (
+	BgHiBlack Attribute = iota + 100
+	BgHiRed
+	BgHiGreen
+	BgHiYellow
+	BgHiBlue
+	BgHiMagenta
+	BgHiCyan
+	BgHiWhite
+)
+
 //Colors is struct base for implements methods the colors package.
 type Colors struct {
-	Fg int
-	Bg int
+	params []Attribute
 }
 
 //NewColorsBasic is return new color instance.
 func NewColors() *Colors {
-	return &Colors{Fg: FgWhite, Bg: BgBlack}
+	return &Colors{
+		params: make([]Attribute, 0),
+	}
 }
 
-//Println print text with colorized in terminal.
-func (c *Colors) Println(content interface{}) {
-	fmt.Println(c.format(content))
+// Add is used to chain SGR parameters. Use as many as parameters to combine.
+func (c *Colors) Add(value ...Attribute) *Colors {
+	c.params = append(c.params, value...)
+	return c
 }
 
-//format return of string formated.
-func (c *Colors) format(content interface{}) string {
-	return fmt.Sprintf("%v[%vm%v[%vm%v%v[0m", prefix, c.Fg, prefix, c.Bg, content, prefix)
+// Println formats using the default formats for its operands and writes to
+// standard output.
+func (c *Colors) Println(a ...interface{}) (int, error) {
+	i, err := fmt.Fprintln(output, c.wrap(fmt.Sprint(a...)))
+	return i, err
+}
 
+//Print formats using the default formats with not line print.
+func (c *Colors) Print(a ...interface{}) (int, error) {
+	i, err := fmt.Fprint(output, c.wrap(fmt.Sprint(a...)))
+	return i, err
+}
+
+// Sprint is just like Print, but returns a string instead of printing it.
+func (c *Colors) Sprint(a ...interface{}) string {
+	return c.wrap(fmt.Sprint(a...))
+}
+
+// Sprintln is just like Println, but returns a string instead of printing it.
+func (c *Colors) Sprintln(a ...interface{}) string {
+	return c.wrap(fmt.Sprintln(a...))
+}
+
+// Sprintf is just like Printf, but returns a string instead of printing it.
+func (c *Colors) Sprintf(format string, a ...interface{}) string {
+	return c.wrap(fmt.Sprintf(format, a...))
+}
+
+// sequence returns a formatted SGR sequence.
+func (c *Colors) sequence() string {
+	format := make([]string, len(c.params))
+	for i, v := range c.params {
+		format[i] = strconv.Itoa(int(v))
+	}
+
+	return strings.Join(format, ";")
+}
+
+// wrap wraps the s string with the colors attributes.
+func (c *Colors) wrap(s string) string {
+	return c.format() + s + c.unformat()
+}
+
+// format return is formated text color for print.
+func (c *Colors) format() string {
+	return fmt.Sprintf("%s[%sm", prefix, c.sequence())
+}
+
+// unformat is responsable by reset format colors.
+func (c *Colors) unformat() string {
+	return fmt.Sprintf("%s[%dm", prefix, Reset)
 }
--- colors_test.go
+++ colors_test.go
@@ -1,8 +1,8 @@
 package colors
 
 import (
-	"fmt"
 	"log"
+	"strings"
 	"testing"
 )
 
@@ -12,12 +12,110 @@ func TestNewColors(t *testing.T) {
 	if c == nil {
 		t.Errorf("TestNewColors(): got -> %v, want: Color{}", c)
 	}
-	text := fmt.Sprintf("\x1b[%vm\x1b[%vm ola \x1b[0m", c.Fg, c.Bg)
-	log.Println(text)
+}
+
+//go test -v -run ^TestAdd
+func TestAdd(t *testing.T) {
+	c := NewColors()
+	c = c.Add(FgGreen)
+	if len(c.params) == 0 {
+		t.Errorf("TestAdd(): got -> %v, want: len > 0", len(c.params))
+	}
+}
+
+//go test -v -run ^TestSequence
+func TestSequence(t *testing.T) {
+	c := NewColors()
+	c = c.Add(FgGreen, Bold)
+	s := c.sequence()
+	if !strings.Contains(s, ";") && len(c.params) > 1 {
+		t.Errorf("TestSequence(): string bad formated.")
+	}
+}
+
+//go test -v -run ^TestWrap
+func TestWrap(t *testing.T) {
+	c := NewColors()
+	c = c.Add(FgMagenta, Bold)
+	s := c.wrap("magenta")
+	if len(s) == 0 && len(c.params) > 1 {
+		t.Errorf("TestWrap(): string bad formated.")
+	}
+	log.Println(s)
+}
+
+//go test -v -run ^TestFormat
+func TestFormat(t *testing.T) {
+	c := NewColors()
+	c = c.Add(FgMagenta)
+	s := c.format()
+	if !strings.HasPrefix(s, "\x1b") {
+		t.Errorf("TestFormat(): string not contains prefix.")
+	}
+	log.Println(s)
+}
+
+//go test -v -run ^TestUnformat
+func TestUnformat(t *testing.T) {
+	c := NewColors()
+	c = c.Add(FgMagenta)
+	s := c.unformat()
+	if !strings.HasPrefix(s, "\x1b") || !strings.HasSuffix(s, "\x1b[0m") {
+		t.Errorf("TestUnformat(): string not contains prefix or suffix correct.")
+	}
 }
 
 //go test -v -run ^TestPrintln
 func TestPrintln(t *testing.T) {
-	c := Colors{Fg: FgRed, Bg: BgBlack}
-	c.Println("red")
+	c := NewColors()
+	c = c.Add(FgGreen, Bold)
+	_, err := c.Println("bold green.")
+	if err != nil {
+		t.Errorf("TestPrintln(): got -> %v,want: nil", err)
+	}
+
+}
+
+//go test -v -run ^TestPrint
+func TestPrint(t *testing.T) {
+	c := NewColors()
+	c = c.Add(FgRed, Bold)
+	_, err := c.Print("bold green.")
+	if err != nil {
+		t.Errorf("TestPrint(): got -> %v,want: nil", err)
+	}
+
+}
+
+//go test -v -run ^TestSprint
+func TestSprint(t *testing.T) {
+	c := NewColors()
+	c = c.Add(FgRed)
+	s := c.Sprint("green.")
+	if len(s) == 0 {
+		t.Errorf("TestSprint(): got -> %v,want: len > 0", len(s))
+	}
+
+}
+
+//go test -v -run ^TestSprintln
+func TestSprintln(t *testing.T) {
+	c := NewColors()
+	c = c.Add(FgYellow)
+	s := c.Sprintln("yellow.")
+	if len(s) == 0 {
+		t.Errorf("TestSprintln(): got -> %v,want: len > 0", len(s))
+	}
+
+}
+
+//go test -v -run ^TestSprintf
+func TestSprintf(t *testing.T) {
+	c := NewColors()
+	c = c.Add(FgCyan)
+	s := c.Sprintf("%v", "cyan.")
+	if len(s) == 0 {
+		t.Errorf("TestSprintln(): got -> %v,want: len > 0", len(s))
+	}
+
 }
